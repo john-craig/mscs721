38b51f83eb0a8e77041c9d936a9b2eeebe639b64
john-craig <john.craig1@marist.edu>
2020-09-29 20:39:08 -0400

    Made progress on running DynamoDB

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+//requires
 'use strict';
 
 var path = require('path');
 var http = require('http');
 
+var AWS = require('aws-sdk/dist/aws-sdk-react-native');
+//var myCredentials = AWS.Credentials('akid', 'secret', 'session')
+
+AWS.config.update({
+    endpoint: "http://localhost:8000",
+    region:'us-east-2',
+    //credentials: myCredentials
+    "accessKeyId": "akid", 
+    "secretAccessKey": "secret",
+});
+
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
+
+//dynamoDB setup
+
+const locationParams = {
+    TableName: "locations",
+    KeySchema: [
+        {
+            AttributeName: "input",
+            KeyType: "STRING"
+        }
+    ],
+    AttributeDefinitions: [
+        {
+            AttributeName: "location",
+            AttributeType: "JSON"
+        }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5, 
+        WriteCapacityUnits: 5
+    }
+}
+
+dynamodb.createTable(locationParams, function(err, data){
+    if (err) console.log(err, err.stack);
+    else     console.log(data);
+})
 
 // swaggerRouter configuration
 var options = {
