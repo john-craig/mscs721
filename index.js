e51296b59eb8385fdb9ff09128c2731657191025
john-craig <john.craig1@marist.edu>
2020-10-02 20:03:45 -0400

    Added dynamodb

--- index.js
+++ index.js
@@ -4,48 +4,70 @@
 var path = require('path');
 var http = require('http');
 
-var AWS = require('aws-sdk/dist/aws-sdk-react-native');
-//var myCredentials = AWS.Credentials('akid', 'secret', 'session')
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
 
 AWS.config.update({
-    endpoint: "http://localhost:8000",
-    region:'us-east-2',
-    //credentials: myCredentials
-    "accessKeyId": "akid", 
-    "secretAccessKey": "secret",
+    endpoint: "http://localhost:8001",
+    region: 'us-east-2',
+    accessKeyId: "akid", 
+    secretAccessKey: "secret",
 });
 
-var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var dynamodb = new AWS.DynamoDB();
 
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
 
 //dynamoDB setup
 
-const locationParams = {
-    TableName: "locations",
-    KeySchema: [
-        {
-            AttributeName: "input",
-            KeyType: "STRING"
-        }
+var locationParams = {
+    TableName : "Locations",
+    KeySchema: [       
+        { AttributeName: "input", KeyType: "HASH"},
+        { AttributeName: "data", KeyType: "SORT" }
     ],
-    AttributeDefinitions: [
-        {
-            AttributeName: "location",
-            AttributeType: "JSON"
-        }
+    AttributeDefinitions: [       
+        { AttributeName: "input", AttributeType: "S" },
+        { AttributeName: "data", AttributeType: "S" }
     ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 5, 
-        WriteCapacityUnits: 5
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
     }
-}
+};
 
-dynamodb.createTable(locationParams, function(err, data){
-    if (err) console.log(err, err.stack);
-    else     console.log(data);
-})
+var concordanceParams = {
+    TableName : "Concordances",
+    KeySchema: [       
+        { AttributeName: "input", KeyType: "HASH"},
+        { AttributeName: "data", KeyType: "SORT" }
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "input", AttributeType: "S" },
+        { AttributeName: "data", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(locationParams, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table: " + locationParams.TableName);
+    }
+});
+
+dynamodb.createTable(concordanceParams, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table: " + concordanceParams.TableName);
+    }
+});
 
 // swaggerRouter configuration
 var options = {
